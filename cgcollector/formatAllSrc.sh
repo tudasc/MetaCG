1ef42b804412e09e4d1a7d3231340c22a6ad7600
Fischler, Yannic <yannic.fischler@tu-darmstadt.de>
2020-08-28 22:27:34 +0200

    Adds CGValidate's edge patching feature
    - CGValidate can insert identified missing edges into the CG.
      Attention: This does not adjust missing edges in the inheritance hierarchy, should they
      exist.
    - Adds cxxopts as dependency

--- cgcollector/formatAllSrc.sh
+++ cgcollector/formatAllSrc.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+find ./lib -iname "*.h" -not -type d -exec clang-format -i {} \;
+find ./lib -iname "*.cpp" -not -type d -exec clang-format -i {} \;
+find ./tools -iname "*.h" -not -type d -exec clang-format -i {} \;
+find ./tools -iname "*.cpp" -not -type d -exec clang-format -i {} \;
+find ./test -iname "*.h" -not -type d -exec clang-format -i {} \;
+find ./test -iname "*.cpp" -not -type d -exec clang-format -i {} \;
 
-find ./lib -regex ".*/*.[cpph]" -not -type d -exec clang-format -i {} \;
-find ./test -regex ".*/*.[cpph]" -not -type d -exec clang-format -i {} \;
-find ./tools -regex ".*/*.[cpph]" -not -type d -exec clang-format -i {} \;
