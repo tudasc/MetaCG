fd6b0841781453bf985a57e6c85c129b18250bb0
Lehr, Jan-Patrick <jan.lehr@sc.tu-darmstadt.de>
2022-02-12 20:02:41 +0100

    Single project README

--- pgis/lib/include/ExtrapAggregatedFunctions.h
+++ pgis/lib/include/ExtrapAggregatedFunctions.h
@@ -1,5 +1,3 @@
-#include <utility>
-
 /**
  * File: ExtrapAggregatedFunctions.h
  * License: Part of the MetaCG project. Licensed under BSD 3 clause license. See LICENSE.txt file at
@@ -7,6 +5,9 @@
  */
 
 #ifndef PGIS_EXTRAPAGGREGATEDFUNCTIONS_H
+#define PGIS_EXTRAPAGGREGATEDFUNCTIONS_H
+
+#include <utility>
 
 /**
  *
@@ -117,7 +118,5 @@ class MaxFunction : public AggregatedFunction {
     return buildString(parameterNames, "MAX(", ", ", ")");
   }
 };
-
-#define PGIS_EXTRAPAGGREGATEDFUNCTIONS_H
 
 #endif  // PGIS_EXTRAPAGGREGATEDFUNCTIONS_H
