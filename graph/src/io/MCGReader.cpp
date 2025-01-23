e4dc8c205c094125e145b2c45c26e715642a0a31
Kreutzer, Sebastian <sebastian.kreutzer@tu-darmstadt.de>
2025-01-23 17:18:36 +0100

    Add a factory function to create the correct reader for each file format

---
+++ graph/src/io/MCGReader.cpp
@@ -0,0 +1,28 @@
+/**
+ * File: MCGReader.cpp
+ * License: Part of the MetaCG project. Licensed under BSD 3 clause license. See LICENSE.txt file at
+ * https://github.com/tudasc/metacg/LICENSE.txt
+ *
+ */
+
+#include "io/MCGReader.h"
+#include "LoggerUtil.h"
+#include "io/VersionThreeMCGReader.h"
+#include "io/VersionTwoMCGReader.h"
+
+namespace metacg::io {
+
+std::unique_ptr<MetaCGReader> createReader(ReaderSource& src) {
+  auto versionStr = src.getFormatVersion();
+  if (versionStr == "2" || versionStr == "2.0") {
+    return std::make_unique<VersionTwoMetaCGReader>(src);
+  } else if (versionStr == "3" || versionStr == "3.0") {
+    return std::make_unique<VersionThreeMetaCGReader>(src);
+  } else {
+    metacg::MCGLogger::instance().getErrConsole()->error("Cannot create reader: format version '{}' is not suppported.",
+                                                         versionStr);
+    return {};
+  }
+}
+
+}  // namespace metacg::io
