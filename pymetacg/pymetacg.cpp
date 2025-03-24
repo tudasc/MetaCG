5a8bf1f8cfe71158f9701f1a024b85343a6e06b4
Peter Arzt <peter.arzt@tu-darmstadt.de>
2025-03-24 17:47:46 +0100

    Use nanobind's low-level API to prevent issue with symbol visibility

--- pymetacg/pymetacg.cpp
+++ pymetacg/pymetacg.cpp
@@ -2,8 +2,13 @@
 
 #include <MCGManager.h>
 
+namespace nb = nanobind;
+using namespace nb::literals;
+
 int add(int a, int b) { return a + b; }
 
 NB_MODULE(pymetacg, m) {
     m.def("add", &add);
+
+    nb::class_<metacg::graph::MCGManager>(m, "MCGManager");
 }
