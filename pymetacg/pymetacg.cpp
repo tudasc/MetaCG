88d43e31c1c7fb47753d9b4bd9730d8a90b6d4b1
Peter Arzt <peter.arzt@tu-darmstadt.de>
2025-03-25 10:51:14 +0100

    Add some basic callgraph handling functions to Python API

--- pymetacg/pymetacg.cpp
+++ pymetacg/pymetacg.cpp
@@ -1,14 +1,49 @@
+#include <string>
+#include <fstream>
+
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/unique_ptr.h>
 
 #include <MCGManager.h>
+#include <Callgraph.h>
+#include <CgNode.h>
+
+#include <io/MCGReader.h>
+#include <io/VersionThreeMCGReader.h>
+#include <io/MCGWriter.h>
 
 namespace nb = nanobind;
 using namespace nb::literals;
 
-int add(int a, int b) { return a + b; }
-
 NB_MODULE(pymetacg, m) {
-    m.def("add", &add);
+    // nb::class_<metacg::graph::MCGManager>(m, "MCGManager");
 
-    nb::class_<metacg::graph::MCGManager>(m, "MCGManager");
+    nb::class_<metacg::CgNode>(m, "CgNode")
+        .def_prop_ro("function_name", &metacg::CgNode::getFunctionName);
+
+    nb::class_<metacg::Callgraph>(m, "Callgraph")
+        .def_static("from_file", [](std::string& path) {
+            metacg::io::FileSource fs(path);
+
+            auto mcgReader = metacg::io::createReader(fs);
+            auto graph = mcgReader->read();
+            return graph;
+        })
+        .def("write_to_file", [](const metacg::Callgraph& self, const std::string& path) {
+            auto mcgWriter = metacg::io::createWriter(3);
+            metacg::io::JsonSink jsonSink;
+            mcgWriter->write(&self, jsonSink);
+            std::ofstream os(path);
+            os << jsonSink.getJson() << std::endl;
+        })
+        .def("has_node", [](const metacg::Callgraph& self, const std::string& name) {
+            return self.hasNode(name);
+        })
+        .def("get_node", [](const metacg::Callgraph& self, const std::string& name) {
+            return self.getNode(name);
+        })
+        .def("clear", [](metacg::Callgraph& self) {
+            self.clear();
+        });
 }
