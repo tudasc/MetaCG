0fb132e0ead27e14d951d23646bb73653b7e0a10
Peter Arzt <peter@arzt-fd.de>
2025-05-22 14:46:10 +0200

    Fix typo pointed out in code review
    Co-authored-by: Jan Patrick Lehr <JanPatrick.Lehr@amd.com>

--- pymetacg/util.h
+++ pymetacg/util.h
@@ -15,7 +15,7 @@
  * Helper type that wraps around CgNode* and keeps additional information about which call graph
  * the node is part of.
  * In the graphlib, call graph nodes do not contain a pointer/reference to the call graph that they
- * are a part of. Hence, one must always call a function of the call class to get a node's callers/callees.
+ * are a part of. Hence, one must always call a function of the call graph to get a node's callers/callees.
  * To allow for more user-friendly access to callers/callees via Python, pymetacg uses this wrapper to
  * attach the graph reference to each CgNode pointer.
  */
