66d0c427da0524d946ab74d89277bc653652d88d
silas.martens <silas.martens@stud.tu-darmstadt.de>
2025-07-11 15:47:06 +0200

    cleaned up testfile

--- tools/cgpatch/test/integration/input/mpi/001_a.cpp
+++ tools/cgpatch/test/integration/input/mpi/001_a.cpp
@@ -9,15 +9,13 @@ int main(int argc, char** argv) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  if (rank == 0) {
-  } else if (rank == 1) {
+  if (rank == 1) {
     if (argc == 1) {
       void* vp = reinterpret_cast<void*>(&secret);
       int (*fptr)(int) = reinterpret_cast<int (*)(int)>(vp);
 
       int result = fptr(7);
-    } else {
-    }
+    } 
   }
 
   MPI_Finalize();
