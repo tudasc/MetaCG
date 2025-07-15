b1d6054d67f995d2264666fdd6a6dfdbb0c36ada
tim.heldmann <tim.heldmann@tu-darmstadt.de>
2025-07-15 16:21:00 +0200

    Removed more function leftovers from AA

--- tools/cgcollector2/include/ASTUtil.h
+++ tools/cgcollector2/include/ASTUtil.h
@@ -9,12 +9,6 @@
 
 #include <clang/AST/ExprCXX.h>
 
-const clang::Type* resolveToUnderlyingType(const clang::Type* ty);
 std::vector<std::string> getMangledNames(clang::Decl const* const nd);
-clang::Stmt* getCalledStmtFromCXXMemberCall(clang::CXXMemberCallExpr* MCE);
-clang::FunctionDecl* getCalledFunctionFromCXXOperatorCallExpr(clang::CXXOperatorCallExpr* OCE);
-std::pair<clang::Stmt*, bool> getImplicitObjectFromCXXMemberCall(clang::CXXMemberCallExpr* MCE);
-void printNamedDeclToConsole(const clang::Decl* D);
-auto getLocation(clang::Decl* decl, clang::Stmt* expr);
 
 #endif  // CGCOLLECTOR2_ASTUTIL_H
--- tools/cgcollector2/src/ASTUtil.cpp
+++ tools/cgcollector2/src/ASTUtil.cpp
@@ -9,18 +9,6 @@
 #include <clang/AST/Mangle.h>
 #include <iostream>
 
-#include <clang/AST/DeclObjC.h>
-
-#include "SharedDefs.h"
-
-const clang::Type* resolveToUnderlyingType(const clang::Type* ty) {
-  if (!ty->isPointerType()) {
-    return ty;
-    // std::cout << "resolveToUnderlyingType: isPointerType" << std::endl;
-  }
-  return resolveToUnderlyingType(ty->getPointeeType().getTypePtr());
-}
-
 
 std::vector<std::string> getMangledNames(clang::Decl const* const nd) {
   if (!nd) {
@@ -34,58 +22,4 @@ std::vector<std::string> getMangledNames(clang::Decl const* const nd) {
     return NG.getAllManglings(nd);
   }
   return {NG.getName(nd)};
-}
-
-clang::Stmt* getCalledStmtFromCXXMemberCall(clang::CXXMemberCallExpr* MCE) {
-  clang::Expr* Callee = MCE->getCallee()->IgnoreParens();
-  if (auto* MemExpr = llvm::dyn_cast<clang::MemberExpr>(Callee)) {
-    return MemExpr;
-  }
-  if (auto* BO = llvm::dyn_cast<clang::BinaryOperator>(Callee)) {
-    if (BO->getOpcode() == clang::BO_PtrMemD || BO->getOpcode() == clang::BO_PtrMemI) {
-      return BO->getRHS();
-    }
-  }
-  return nullptr;
-}
-
-clang::FunctionDecl* getCalledFunctionFromCXXOperatorCallExpr(clang::CXXOperatorCallExpr* OCE) {
-  auto Called = OCE->getCallee()->IgnoreParenCasts();
-  if (auto* DeclRefExpr = llvm::dyn_cast<clang::DeclRefExpr>(Called)) {
-    if (auto* FunctionDecl = llvm::dyn_cast<clang::FunctionDecl>(DeclRefExpr->getDecl())) {
-      return FunctionDecl;
-    }
-  }
-  return nullptr;
-}
-
-std::pair<clang::Stmt*, bool> getImplicitObjectFromCXXMemberCall(clang::CXXMemberCallExpr* MCE) {
-  clang::Expr* Callee = MCE->getCallee()->IgnoreParens();
-  if (const auto* MemExpr = llvm::dyn_cast<clang::MemberExpr>(Callee))
-    return {MemExpr->getBase(), MemExpr->isArrow()};
-  if (const auto* BO = llvm::dyn_cast<clang::BinaryOperator>(Callee)) {
-    if (BO->getOpcode() == clang::BO_PtrMemD) {
-      return {BO->getLHS(), false};
-    } else if (BO->getOpcode() == clang::BO_PtrMemI) {
-      return {BO->getLHS(), true};
-    }
-  }
-
-  return {nullptr, false};
-}
-
-void printNamedDeclToConsole(const clang::Decl* D) {
-  assert(D);
-#ifndef NDEBUG
-  auto fd = llvm::dyn_cast<clang::NamedDecl>(D);
-  if (fd) {
-    std::cout << fd->getNameAsString() << std::endl;
-  }
-#endif
-}
-
-auto getLocation(clang::Decl* decl, clang::Stmt* expr) {
-  const auto& ctx = decl->getASTContext();
-  const auto& man = ctx.getSourceManager();
-  return expr->getSourceRange().getBegin().printToString(man);
 }
