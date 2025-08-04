7874d330fa5621c1e87e10b0bad88813d1e07951
Sebastian Kreutzer <SebastianKreutzer@gmx.net>
2025-08-04 13:36:04 +0200

    Add script for test graph checking

--- checkTests.sh
+++ checkTests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# This script goes through all test call graphs and performs basic checks using cgformat.
+
 cgformat="${1:-cgformat}"
+
+if [[ ! -x "$(which $cgformat)" ]]; then
+  echo "Need to pass 'cgformat' executable as argument - exiting."
+  exit 1
+fi
 
 num_failures=0
 
@@ -12,17 +19,28 @@ for dir in "${target_folders[@]}"; do
   full_dir="$script_dir/$dir"
   graph_tests=$(find "$full_dir" -regextype posix-extended -type f -regex '.*\.(gt)?(ipcg|mcg)$')
 
-  echo "$graph_tests"
   echo "Checking ${#graph_tests[@]} MetaCG files in $dir..."
 
   for t in $graph_tests; do
     echo "Checking file: $t"
+
+    # Skipping v1 files
     grep -q '"_MetaCG"' $t
     if [[ $? -ne 0 ]]; then
       echo "File $t has no _MetaCG field - skipping..."
       continue
     fi
+
+    # Skipping null graphs (because otherwise the reader in cgformat fails).
+    grep -q '"_CG": null' $t
+    if [[ $? -eq 0 ]]; then
+      echo "File $t contains 'null' call graph - skipping..."
+      continue
+    fi
+
+    # Invoke cgformat to check for canonical origin prefix
     "$cgformat" --origin_prefix "/opt/metacg" "$t" > /dev/null
+
     if [[ $? -ne 0 ]]; then
       echo "Formatting check failed"
       ((num_failures++))
