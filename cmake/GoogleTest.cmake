ad0be14464ad5e46bd4c1639a0692a5cb22e1e81
Peter Arzt <peter.arzt@tu-darmstadt.de>
2025-08-07 15:23:37 +0200

    [NFC] Update to googletest v1.17.0

--- cmake/GoogleTest.cmake
+++ cmake/GoogleTest.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 FetchContent_Declare(
-  googletest URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+  googletest URL https://github.com/google/googletest/releases/download/v1.17.0/googletest-1.17.0.tar.gz
 )
 FetchContent_MakeAvailable(googletest)
 
