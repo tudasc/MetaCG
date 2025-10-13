708daf8225693c38dc9543f76d9aa414bf6d528f
silas-martens <158048386+silas-martens@users.noreply.github.com>
2025-10-13 15:48:41 +0200

    Rename set
    Co-authored-by: Jan Patrick Lehr <jp.lehr@gmail.com>

--- tools/cgdiff/include/NodeDiff.h
+++ tools/cgdiff/include/NodeDiff.h
@@ -22,7 +22,7 @@ struct NodeDiff : Diff {
 
   NodeDiff() = default;
   NodeDiff(std::string name_, std::vector<std::string> diffType_ = {}, std::vector<EdgeDiff> edgeDiffs_ = {},
-           std::unordered_set<std::string> metaA_ = {}, std::unordered_set<std::string> metaB_ = {})
+           std::unordered_set<std::string> metaA = {}, std::unordered_set<std::string> metaB = {})
       : name(std::move(name_)),
         diffType(std::move(diffType_)),
         edgeDiffs(std::move(edgeDiffs_)),
