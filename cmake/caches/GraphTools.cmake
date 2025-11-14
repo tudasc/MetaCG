6a0a35f1cacc3a26ccf5a261d790c28a886edcd7
JP Lehr <jp.lehr@gmail.com>
2025-11-14 12:48:50 +0100

    [CMake] Add cache files for convenience
    Adding two CMake cache files to make building MetaCG more convenient.
    
    One cache is a full build that requires the external dependencies
    to be installed via the build_submodules.sh script.
    
    The other cache is a build of the "new" graph tools that does not
    depend on the external components.

---
+++ cmake/caches/GraphTools.cmake
@@ -0,0 +1,7 @@
+# This is a CMake cache file for a full build
+set(CMAKE_BUILD_TYPE Release CACHE STRING "")
+
+# Enable all features from MetaCG
+set(METACG_BUILD_CGCOLLECTOR OFF CACHE BOOL "")
+set(METACG_BUILD_PGIS OFF CACHE BOOL "")
+set(METACG_BUILD_GRAPH_TOOLS ON CACHE BOOL "")
