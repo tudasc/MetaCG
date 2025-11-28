b2e4f5d097306f1009af66b0836d49ca1f8aca84
silas.martens <silas.martens@stud.tu-darmstadt.de>
2025-11-28 20:39:10 +0100

    fix formatting

--- tools/cgcollector2/test/input/cxxRecordCalls/0018.cpp
+++ tools/cgcollector2/test/input/cxxRecordCalls/0018.cpp
@@ -1,8 +1,8 @@
 /**
- * File: MemberFunctions.cpp
- * License: Part of the MetaCG project. Licensed under BSD 3 clause license. See LICENSE.txt file at
- * https://github.com/tudasc/metacg/LICENSE.txt
- */
+* File: MemberFunctions.cpp
+* License: Part of the MetaCG project. Licensed under BSD 3 clause license. See LICENSE.txt file at
+* https://github.com/tudasc/metacg/LICENSE.txt
+*/
 
 struct A {
   int foo() { return 6; }
