8645771fb661bf0ee8e28746f8bcb9a12c2686ca
silas-martens <158048386+silas-martens@users.noreply.github.com>
2025-12-05 14:38:12 +0100

    Check for lower than 18
    Co-authored-by: TimHeldmann <tim.heldmann@tu-darmstadt.de>

--- tools/cgcollector2/fileInfoDemoPlugin/FileInfoMetadataPlugin.cpp
+++ tools/cgcollector2/fileInfoDemoPlugin/FileInfoMetadataPlugin.cpp
@@ -24,7 +24,7 @@ struct FileInfoMetadataPlugin : Plugin {
     auto& astCtx = functionDecl->getASTContext();
     const auto fullSrcLoc = astCtx.getFullLoc(sourceLocation);
 
-#if LLVM_VERSION_MAJOR < 19
+#if LLVM_VERSION_MAJOR < 18
     const auto fileEntry = fullSrcLoc.getFileEntry();
 #else
     const auto fileEntry = fullSrcLoc.getFileEntryRef();
