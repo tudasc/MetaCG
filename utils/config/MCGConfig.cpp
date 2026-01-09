8cd020d72bb7967ee976e03bbc2abe2e56b3b9c1
Sebastian Kreutzer <sebastian.kreutzer@tu-darmstadt.de>
2026-01-09 15:37:03 +0100

    [CaGe] Refactoring metacg-config options and fixing CMake for LLVM<15

--- utils/config/MCGConfig.cpp
+++ utils/config/MCGConfig.cpp
@@ -20,11 +20,10 @@ int main(int argc, char** argv) {
   options.add_options("commands")("v,version", "Prints the version of this MetaCG installation.")(
       "revision", "Prints the revision hash of this MetaCG installation")(
       "prefix", "Prints the installation prefix of this MetaCG installation.")(
-      "ldflags", "Prints the necessary ld flags.")(
-      "cflags", "Prints the necessary C compile flags.")(
-      "cxxflags", "Prints the necessary C++ compile flags.")(
-      "cage", "Enables the CaGe LTO plugin (requires graph tools).")(
-      "pass-option", "Sets a pass option", cxxopts::value<std::vector<std::string>>())(
+      "cage-ldflags", "Prints the necessary ld flags.")(
+      "cage-cflags", "Prints the necessary C compile flags.")(
+      "cage-cxxflags", "Prints the necessary C++ compile flags.")(
+      "cage-pass-option", "Sets a pass option", cxxopts::value<std::vector<std::string>>())(
       "h,help", "Print help");
   // clang-format on
 
@@ -35,24 +34,24 @@ int main(int argc, char** argv) {
     return EXIT_SUCCESS;
   }
 
+  bool cageOptionSet = result.count("cage-ldflags") +
+                    result.count("cage-cflags") + result.count("cage-cxxflags") + result.count("cage-pass-option");
+
+#ifndef HAVE_CAGE
+  if (cageOptionSet) {
+    std::cerr << "CaGe is currently not available. Configure with -DMETACG_BUILD_GRAPH_TOOLS to build it.\n";
+    return EXIT_FAILURE;
+  }
+#endif
+
   // Exactly one of these is allowed at the same time
-  int optCount = result.count("version") + result.count("revision") + result.count("prefix") + result.count("ldflags") +
-                 result.count("cflags") + result.count("cxxflags");
+  int optCount = result.count("version") + result.count("revision") + result.count("prefix") + (cageOptionSet ? 1 : 0);
   if (optCount == 0) {
     std::cerr << "Error: No command specified.\n";
     return EXIT_FAILURE;
   } else if (optCount > 1) {
     std::cerr << "Warning: Multiple mutually exclusive commands specified. Only one of them will be processed.\n";
   }
-
-  // Checking for consistency
-  bool useCaGe = result.contains("cage");
-#ifndef HAVE_GRAPH_TOOLS
-  if (useCaGe) {
-    std::cerr << "CaGe is currently not available. Configure with -DMETACG_BUILD_GRAPH_TOOLS to enable it.\n";
-    return EXIT_FAILURE;
-  }
-#endif
 
   if (result.contains("version")) {
     std::cout << MetaCG_VERSION_MAJOR << "." << MetaCG_VERSION_MINOR;
@@ -69,35 +68,30 @@ int main(int argc, char** argv) {
     return EXIT_SUCCESS;
   }
 
+#ifdef HAVE_CAGE
   std::stringstream passOptsStream;
-  if (result.count("pass-option")) {
-    const auto& passOpts = result["pass-option"].as<std::vector<std::string>>();
+  if (result.count("cage-pass-option")) {
+    const auto& passOpts = result["cage-pass-option"].as<std::vector<std::string>>();
     for (auto& opt : passOpts) {
       passOptsStream << " -Wl,-mllvm=" << opt << " ";
     }
   }
   std::string processedPassOpts = passOptsStream.str();
 
-  if (result.contains("cflags")) {
-    if (useCaGe) {
+  if (result.contains("cage-cflags")) {
       std::cout << " -flto ";
-    }
   }
 
-  if (result.contains("cxxflags")) {
-    if (useCaGe) {
+  if (result.contains("cage-cxxflags")) {
       std::cout << " -flto ";
-    }
   }
 
-  if (result.contains("ldflags")) {
-#ifdef HAVE_GRAPH_TOOLS
-    if (useCaGe) {
-      std::cout << " -flto -fuse-ld=lld -Wl,-mllvm=-load=" << CAGE_PLUGIN << " -Wl,--load-pass-plugin=" << CAGE_PLUGIN
-                << processedPassOpts << " ";
-    }
+  if (result.contains("cage-ldflags")) {
+    std::cout << " -flto -fuse-ld=lld -Wl,-mllvm=-load=" << CAGE_PLUGIN << " -Wl,--load-pass-plugin=" << CAGE_PLUGIN
+              << processedPassOpts << " ";
+
+  }
 #endif
-  }
 
   return EXIT_SUCCESS;
 }
