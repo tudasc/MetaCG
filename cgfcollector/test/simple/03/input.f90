6e5efe7cb1f0aaca9505f355c140471c7ac5f7a2
gomfol12 <info@marekb.de>
2026-02-05 15:32:45 +0100

    better test case for finalizers

--- cgfcollector/test/simple/03/input.f90
+++ cgfcollector/test/simple/03/input.f90
@@ -39,6 +39,26 @@ contains
 
 end module mod
 
+module mod_use
+    use mod
+
+    implicit none
+
+contains
+
+    subroutine func_calls_final()
+        type(polynomial), allocatable :: q
+
+        q = polynomial([2., 3., 1., 0., 0.])
+        call q%print_polynomial()
+    end subroutine func_calls_final
+
+    subroutine func_does_not_call_final()
+        type(polynomial), allocatable :: q
+    end subroutine func_does_not_call_final
+
+end module mod_use
+
 program main
     use mod
     implicit none
@@ -49,5 +69,7 @@ program main
         q = polynomial([2., 3., 1., 0., 0.])
         call q%print_polynomial()
     end block work
+
+    ! sould not call final because main. see 7.5.6.4 (https://j3-fortran.org/doc/year/23/23-007r1.pdf) and (https://j3-fortran.org/doc/year/10/10-158r1.txt)
 
 end program main
