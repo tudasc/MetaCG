715706b95f6d71b679dd4120e3f9500a72badc77
gomfol12 <info@marekb.de>
2026-02-05 15:32:44 +0100

    better test case for operator overloading and defined operators

--- cgfcollector/test/simple/06/main.f90
+++ cgfcollector/test/simple/06/main.f90
@@ -5,21 +5,38 @@ module mod
     type, abstract :: sortable
     contains
         procedure(compare), deferred :: less_then
+        procedure(not), deferred :: not_impl
         generic :: operator(<) => less_then
+        generic :: operator(.NOT.) => not_impl
     end type sortable
 
     interface
         pure logical function compare(this, other)
             import :: sortable
+            implicit none
             class(sortable), intent(in) :: this, other
         end function compare
+        pure logical function not(this)
+            import :: sortable
+            implicit none
+            class(sortable), intent(in) :: this
+        end function not
     end interface
 
     type, extends(sortable) :: integer_sortable
         integer :: value
     contains
         procedure :: less_then => less_than_integer
+        procedure :: not_impl => not_impl_integer
     end type integer_sortable
+
+    interface operator(.NEGX.)
+        module procedure negx
+    end interface
+
+    interface operator(+)
+        module procedure add_stuff
+    end interface
 
 contains
     pure logical function less_than_integer(this, other)
@@ -39,6 +56,28 @@ contains
         ! less_than_integer = this%value < other_int%value
 
     end function less_than_integer
+
+    pure logical function not_impl(this)
+        class(sortable), intent(in) :: this
+        not_impl = .NOT. this%less_then(this)
+    end function not_impl
+
+    pure logical function not_impl_integer(this)
+        class(integer_sortable), intent(in) :: this
+        not_impl_integer = .NOT. this < this
+    end function not_impl_integer
+
+    function negx(x) result(res)
+        real, intent(in) :: x
+        real :: res
+        res = -x
+    end function negx
+
+    function add_stuff(x, y) result(res)
+        type(integer_sortable), intent(in) :: x, y
+        type(integer_sortable) :: res
+        res%value = x%value + y%value
+    end function add_stuff
 end module mod
 
 program main
@@ -49,6 +88,9 @@ program main
     class(sortable), allocatable :: a, b
 
     type(integer_sortable) :: c, d
+    type(integer_sortable) :: res
+
+    real :: e = 5.0, f
 
     c%value = 5
     d%value = 10
@@ -61,6 +103,12 @@ program main
     else
         print *, "a is not less than b"
     end if
+
+    f = .NEGX.e
+    print *, f
+
+    res = c + d
+    print *, "Result of addition: ", res%value
 
 end program main
 
