1e40dfb1975dcffd17d5089d8d3125b7309ae5bb
gomfol12 <info@marekb.de>
2026-02-05 15:32:42 +0100

    WIP: impl parsing for function overriding in derived types. And broke some tests.

--- cgfcollector/src/main.cpp
+++ cgfcollector/src/main.cpp
@@ -52,18 +52,12 @@ class CollectCallersAndCallees : public Fortran::frontend::PluginParseTreeAction
       inFunctionOrSubroutineSubProgram = true;
       return true;
     }
-    bool Post(const Fortran::parser::FunctionSubprogram&) {
-      inFunctionOrSubroutineSubProgram = false;
-      return true;
-    }
+    void Post(const Fortran::parser::FunctionSubprogram&) { inFunctionOrSubroutineSubProgram = false; }
     bool Pre(const Fortran::parser::SubroutineSubprogram&) {
       inFunctionOrSubroutineSubProgram = true;
       return true;
     }
-    bool Post(const Fortran::parser::SubroutineSubprogram&) {
-      inFunctionOrSubroutineSubProgram = false;
-      return true;
-    }
+    void Post(const Fortran::parser::SubroutineSubprogram&) { inFunctionOrSubroutineSubProgram = false; }
 
     void Post(const Fortran::parser::ExecutionPart& e) {
       if (!inFunctionOrSubroutineSubProgram && !inMainProgram)
@@ -99,14 +93,53 @@ class CollectCallersAndCallees : public Fortran::frontend::PluginParseTreeAction
 
       // if called from a object with %
       if (auto* compRef = std::get_if<Fortran::parser::ProcComponentRef>(&p.u)) {
-        if (!compRef->v.thing.component.symbol)
+        auto* symbolComp = compRef->v.thing.component.symbol;
+        if (!symbolComp)
           return;
 
-        // ignore intrinsic functions TODO check
-        // if (compRef->v.thing.component.symbol->attrs().test(Fortran::semantics::Attr::INTRINSIC))
-        //   return;
-
         callee = Fortran::lower::mangle::mangleName(*compRef->v.thing.component.symbol);
+
+        auto* baseName = std::get_if<Fortran::parser::Name>(&compRef->v.thing.base.u);
+        if (!baseName || !baseName->symbol)
+          return;
+        auto* symbolBase = baseName->symbol;
+
+        // handle derived types edges TODO test
+        if (auto* type = symbolBase->GetType()) {
+          if (auto* derived = type->AsDerived()) {
+            auto& typeSymbol = derived->typeSymbol();
+
+            auto findTypeIt = std::find_if(types.begin(), types.end(),
+                                           [&typeSymbol](const type_t& t) { return t.type == &typeSymbol; });
+            if (findTypeIt == types.end())
+              return;  // TODO would i want to return here? yes this breaks the nesting tests
+
+            // handle base type
+            auto& baseProcs = findTypeIt->procedures;
+
+            auto baseProcIt = std::find_if(baseProcs.begin(), baseProcs.end(), [&symbolComp](const auto& p) {
+              return p.first->name() == symbolComp->name();
+            });
+            if (baseProcIt == baseProcs.end())
+              return;  // also here
+
+            edges.emplace_back(functionNames.back(), Fortran::lower::mangle::mangleName(*baseProcIt->second));
+
+            // handle derived types
+            for (const auto& t : types) {
+              if (t.extendsFrom != &typeSymbol)
+                continue;
+
+              auto dProcIt = std::find_if(t.procedures.begin(), t.procedures.end(), [&symbolComp](const auto& p) {
+                return p.first->name() == symbolComp->name();
+              });
+              if (dProcIt == t.procedures.end())
+                continue;
+
+              edges.emplace_back(functionNames.back(), Fortran::lower::mangle::mangleName(*dProcIt->second));
+            }
+          }
+        }
       }
 
       if (functionNames.empty())
@@ -115,7 +148,84 @@ class CollectCallersAndCallees : public Fortran::frontend::PluginParseTreeAction
       edges.emplace_back(functionNames.back(), callee);
     }
 
+    // TODO: handle destructors (finalizers)
+    void Post(const Fortran::parser::TypeDeclarationStmt& t) {
+      // TODO they dont need to hold intent
+      // const auto& attrs = std::get<std::list<Fortran::parser::AttrSpec>>(t.t);
+      // for (const auto& attr : attrs) {
+      //   if (std::holds_alternative<Fortran::parser::IntentSpec>(attr.u)) {
+      //     return;
+      //   }
+      // }
+
+      // const auto& entityDecls = std::get<std::list<Fortran::parser::EntityDecl>>(t.t);
+      // for (const auto& entity : entityDecls) {
+      //   const auto& name = std::get<Fortran::parser::ObjectName>(entity.t);
+      //   if (!name.symbol)
+      //     continue;
+
+      //   // TODO they dont need to hold intent
+      //   if (name.symbol->attrs().test(Fortran::semantics::Attr::INTENT_IN))
+      //     continue;  // skip intent in
+
+      //   llvm::outs() << "Found type declaration: " << name.symbol->name().ToString() << "\n";
+      //   if (auto* type = name.symbol->GetType()) {
+      //     if (auto* derived = type->AsDerived()) {
+      //       if (derived->HasDestruction()) {
+      //         llvm::outs() << "Found derived type with destruction: " << name.symbol->name().ToString() << "\n";
+      //       }
+      //     }
+      //   }
+      // }
+    }
+
+    // type def
+    bool Pre(const Fortran::parser::DerivedTypeDef&) {
+      inDerivedTypeDef = true;
+      types.emplace_back();
+
+      return true;
+    }
+    void Post(const Fortran::parser::DerivedTypeDef&) { inDerivedTypeDef = false; }
+
+    // type stmt like type [, extends(...)] :: body (not exhaustive and not extends)
+    void Post(const Fortran::parser::DerivedTypeStmt& t) {
+      if (!inDerivedTypeDef)
+        return;
+
+      auto& currentType = types.back();
+      const auto& name = std::get<Fortran::parser::Name>(t.t);
+      currentType.type = name.symbol;
+    }
+
+    // type attrs like extends
+    void Post(const Fortran::parser::TypeAttrSpec& a) {
+      if (!inDerivedTypeDef)
+        return;
+
+      auto& currentType = types.back();
+      if (std::holds_alternative<Fortran::parser::TypeAttrSpec::Extends>(a.u)) {
+        const auto& extends = std::get<Fortran::parser::TypeAttrSpec::Extends>(a.u);
+        currentType.extendsFrom = extends.v.symbol;
       }
+    }
+
+    // procedures in type defs
+    void Post(const Fortran::parser::TypeBoundProcDecl& d) {
+      if (!inDerivedTypeDef)
+        return;
+
+      auto& name = std::get<Fortran::parser::Name>(d.t);
+      if (!name.symbol)
+        return;
+
+      auto& optname = std::get<std::optional<Fortran::parser::Name>>(d.t);
+      if (!optname || !optname->symbol) {
+        return;
+      }
+
+      auto& currentType = types.back();
+      currentType.procedures.emplace_back(name.symbol, optname->symbol);
     }
 
    private:
@@ -126,6 +236,14 @@ class CollectCallersAndCallees : public Fortran::frontend::PluginParseTreeAction
     std::vector<std::string> functionNames;
     bool inFunctionOrSubroutineSubProgram = false;
     bool inMainProgram = false;
+    bool inDerivedTypeDef = false;
+
+    typedef struct type {
+      Fortran::semantics::Symbol* type;
+      Fortran::semantics::Symbol* extendsFrom;
+      std::vector<std::pair<Fortran::semantics::Symbol*, Fortran::semantics::Symbol*>> procedures;
+    } type_t;
+    std::vector<type> types;
   };
 
   void executeAction() override {
@@ -169,6 +287,9 @@ class CollectCallersAndCallees : public Fortran::frontend::PluginParseTreeAction
     dotGen.generate();
 
     llvm::SmallString<128> outputPath(getInstance().getFrontendOpts().outputFile);
+    if (outputPath.empty()) {
+      outputPath = getCurrentFile();
+    }
     llvm::sys::path::replace_extension(outputPath, "dot");
     std::error_code ec;
     llvm::raw_fd_ostream out(outputPath.str(), ec);
