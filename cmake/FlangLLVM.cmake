e9aff3099d123493c31d1bdfcd190a81392679d9
gomfol12 <info@marekb.de>
2026-02-05 15:33:02 +0100

    don't link against flangFrontendTool

--- cmake/FlangLLVM.cmake
+++ cmake/FlangLLVM.cmake
@@ -26,5 +26,5 @@ function(add_flang target)
     NO_DEFAULT_PATH
   )
 
-  target_link_libraries(${target} PUBLIC flangFrontendTool)
+  # target_link_libraries(${target} PUBLIC flangFrontendTool)
 endfunction()
